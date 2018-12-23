eebc2f4a43b65e6ec8ac58216c0a669774b28fad
MrHuggs <dude@dudaduda.com>
2018-12-22 22:47:57 -0800

    Created
    Created project.

---
+++ Assets/LookAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+	public GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+		transform.LookAt(Target.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ShowOmega.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOmega : MonoBehaviour
+{
+	public GameObject Target;
+	Rigidbody TargetBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+		TargetBody = Target.GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = Target.transform.position;
+
+		var w = TargetBody.angularVelocity;
+		
+		Quaternion q = new Quaternion();
+		q.SetFromToRotation(Vector3.up, TargetBody.angularVelocity);
+
+		transform.rotation = q;
+
+	}
+}
