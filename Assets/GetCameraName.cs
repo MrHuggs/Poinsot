97656dd766ac510d4d534082089747003699cc8e
MrHuggs <dude@dudaduda.com>
2018-12-23 10:21:28 -0800

    Add cameral label
    Put labels in the various cameras.
    Ideally, the views would be a prefab.

---
+++ Assets/GetCameraName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetCameraName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		var r = transform.root;
+		var c = GetComponent<Text>();
+		c.text = r.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
